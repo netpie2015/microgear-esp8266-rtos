006d65df1f7209f9d6e8f6e59ab74dd5b620a6ac
Panitan <panitan_m@kkumail.com>
2017-06-20 19:10:06 +0700

    add time function

--- Microgear/ESPTime.c
+++ Microgear/ESPTime.c
@@ -1,9 +1,18 @@
 #include "ESPTime.h"
 
-void setTime(uint32_t ts) {
+uint32_t ts_starttime;
+uint32_t ts_localtime;
 
+uint32_t secs(void) {
+	uint32_t micros = system_get_time();
+	return (micros / 1000000);
+}
+
+void setTime(uint32_t ts) {
+	ts_starttime = ts;
+	ts_localtime = secs();
 }
 
 uint32_t getTime() {
-
+	return (secs() + ts_starttime - ts_localtime);
 }
