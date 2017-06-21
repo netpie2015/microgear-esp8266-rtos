d55dc6c48c3940815cc19024abad0fe8c1b35203
Chavee Issariyapat <i@chavee.com>
2017-06-21 15:40:10 +0700

    small fix

--- Microgear/TokenStore.h
+++ Microgear/TokenStore.h
@@ -5,21 +5,20 @@
 #include "c_types.h"
 #include "spi_flash.h"
 
-#define ESP_PARAM_SEC_A		0x79
-#define ESP_PARAM_SEC_B		0x7A
+#define ESP_FLASH_SEC            0x79
 
-#define TOKENSIZE                  		16
-#define TOKENSECRETSIZE            		32
-#define ENDPOINTSIZE					200
+#define TOKENSIZE                16
+#define TOKENSECRETSIZE          32
+#define ENDPOINTSIZE             200
 
 struct token_struct{
-	char type;
-	char token[TOKENSIZE+1];
-	char secret[TOKENSECRETSIZE+1];
-	char saddr[ENDPOINTSIZE+1];
-	uint16_t sport;
-	char flag;
-	char dummy[1]; // make struct size devidable by 4
+    char type;
+    char token[TOKENSIZE+1];
+    char secret[TOKENSECRETSIZE+1];
+    char saddr[ENDPOINTSIZE+1];
+    uint16_t sport;
+    char flag;
+    char dummy[1]; // make struct size devidable by 4
 };
 
 typedef struct token_struct Token;
