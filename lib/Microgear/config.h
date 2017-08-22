591a13dd607f97a54f2b00b9344b09e3ddede90c
Chavee Issariyapat <i@chavee.com>
2017-08-22 15:13:26 +0700

    set default topic max size to 96

--- lib/Microgear/config.h
+++ lib/Microgear/config.h
@@ -1,5 +1,9 @@
+#ifndef _CONFIG_
+#define _CONFIG_
 #define DEBUG
 
-#define PUBSUBQUEUE_TOPICSIZE      48
+#define PUBSUBQUEUE_TOPICSIZE      96
 #define PUBSUBQUEUE_PAYLOADSIZE    256
 #define PUBSUBQUEUE_LENGTH          3
+
+#endif
